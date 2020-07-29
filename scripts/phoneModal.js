475b28a0ae1015c6757c09005e64e5d230cea243
trenando <48501601+trenando@users.noreply.github.com>
2020-07-29 20:13:35 +0300

    last commit + add readme information

--- scripts/phoneModal.js
+++ scripts/phoneModal.js
@@ -4,12 +4,26 @@ const openModal = $('.category-phone');
 const itemHover = $('.toptitle__modal');
 const shadow = $('.header__shadow');
 
-openModal.hover(function () {
+openModal.mouseenter(function () {
     itemHover.css("display", "block");
-    openModal.css("background-color","#5db975");
-    shadow.css("display","initial");
-}, function () {
-    itemHover.css("display", "none");
-    openModal.css("background-color","#34a853");
-    shadow.css("display", "none");
+    openModal.css("background-color", "#5db975");
+    shadow.css("display", "initial");
 });
+
+openModal.mouseleave(function (e) {
+    const leaveTarget = e.toElement || e.relatedTarget;
+    if ($(leaveTarget).is(itemHover) || $.contains(itemHover, $(leaveTarget))) {
+        return;
+    }
+    hideModal();
+});
+
+itemHover.mouseleave(function(){
+    hideModal();
+});
+
+function hideModal(){
+    itemHover.css("display", "none");
+    openModal.css("background-color", "#34a853");
+    shadow.css("display", "none");
+}
